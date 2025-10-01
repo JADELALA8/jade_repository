cbc521ae2b7edad3e10f03ba6339f7d361bbbd90
JADELALA8 <yumeng.apply@outlook.com>
2025-10-01 12:12:08 +0100

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# Lab2 - Exercise 1
+name = input("Enter your name: ")
+num = int(input("Enter a number: "))
+
+if num < 10:
+    for i in range(num):
+        print(name)
+else:
+    for i in range(3):
+        print("Too high")
